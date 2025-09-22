96b0426f9f7c4b7dd1c22ee879f31e85995dfa54
Kerolos Samoul <https://github.com/kokomagdy027>
2025-09-23 01:40:20 +0300

    -error

--- koko/Program.cs
+++ koko/Program.cs
@@ -11,6 +11,7 @@
             Console.WriteLine("kokokokokoko");
             Console.WriteLine("llllllllllll");
             Console.WriteLine("gggggggggggggggggg");
+            kf
         }
     }
 }
